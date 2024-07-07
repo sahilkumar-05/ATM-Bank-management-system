b0f35f233d4b95c8899debd2318e8d95d1305912
sahilkumar-05 <129257408+sahilkumar-05@users.noreply.github.com>
2024-07-07 23:38:44 +0500

    Signup page start

--- src/project/pkg1/Signup.java
+++ src/project/pkg1/Signup.java
@@ -1,7 +1,13 @@
 
 package project.pkg1;
-
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class Signup {
-    
+    Signup(){
+        
+        
+    }
 }
